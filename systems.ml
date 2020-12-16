f6b09a16cb4ac1174481564a2c5ed4a9adc70027
BOUARAH Romain <romain.bouarah@etu.univ-paris-diderot.fr>
2020-12-16 17:40:34 +0100

    Change Turtle coding style to functional style

--- systems.ml
+++ systems.ml
@@ -14,46 +14,54 @@ type 's system = {
 
 (** Put here any type and function implementations concerning systems *)
 
-let rec draw_simb interp symb = symb |> interp |> Turtle.exec
-and
-draw_seq interp seq = match seq with
-  | [] -> ()
-  | x :: seq -> draw_word interp x; draw_seq interp seq
-and
-draw_branch interp branch =
-  let _ = Turtle.exec [Store] in
-  let _ = draw_word interp branch in
-  Turtle.exec [Restore]
-and
-draw_word interp w = match w with
-  | Symb sym -> draw_simb interp sym
-  | Branch bra -> draw_branch interp bra
-  | Seq seq -> draw_seq interp seq
+let rec draw_symb interp symb turtle =
+  symb |> interp |> Turtle.exec turtle
 
-let draw sys = draw_word sys.interp sys.axiom
+and draw_seq interp seq turtle =
+  match seq with
+  | [] -> turtle
+  | x :: seq -> let t' = draw_word interp x turtle in
+                draw_seq interp seq t'
+
+and draw_branch interp branch turtle =
+  let t1 = Turtle.exec turtle [Store] in
+  let t2 = draw_word interp branch t1 in
+  Turtle.exec t2 [Restore]
+
+and draw_word interp word turtle =
+  match word with
+  | Symb sym -> draw_symb interp sym turtle
+  | Branch bra -> draw_branch interp bra turtle
+  | Seq seq -> draw_seq interp seq turtle
+
+let draw_system sys =
+  let turtle = Turtle.create_turtle () in
+  let _ = draw_word sys.interp sys.axiom turtle in
+  ();;
+
 
 
 let rec next_symb rules symb i =
   if i = 0 then Symb symb
   else next_word rules (rules symb) (i-1)
-and
-next_seq rules seq i =
+
+and next_seq rules seq i =
   let rec next_seq_list seq = match seq with
     | [] -> []
     | x :: l -> (next_word rules x i) :: (next_seq_list l)
   in Seq (next_seq_list seq)
-and
-next_branch rules branch i =
+
+and next_branch rules branch i =
   if i = 0 then Branch branch
   else
     Branch (next_word rules branch i)
-and
-next_word rules word i =
+
+and next_word rules word i =
   if i = 0 then word
   else match word with
-    | Symb sym -> next_symb rules sym i
-    | Branch bra -> next_branch rules bra i
-    | Seq seq -> next_seq rules seq i
+       | Symb sym -> next_symb rules sym i
+       | Branch bra -> next_branch rules bra i
+       | Seq seq -> next_seq rules seq i
 
 let next sys i =
   { sys with axiom = next_word sys.rules sys.axiom i}
--- turtle.ml
+++ turtle.ml
@@ -1,63 +1,79 @@
-
 type command =
-| Line of int
-| Move of int
-| Turn of int
-| Store
-| Restore
+  | Line of int
+  | Move of int
+  | Turn of int
+  | Store
+  | Restore
 
 type position = {
-  x: float;      (** position x *)
-  y: float;      (** position y *)
-  a: int;        (** angle of the direction *)
-}
+    x: float;      (** position x *)
+    y: float;      (** position y *)
+    a: int;        (** angle of the direction *)
+  }
 
 (** Put here any type and function implementations concerning turtle *)
 type turtle = {
-  mutable pos: position ;
-  states: position Stack.t ;
-}
+    pos : position;
+    states : position list;
+  }
 
-let t = { pos = { x = 0.; y = 0.; a = 0}; states = Stack.create ()};;
 
-let init () =
-  Graphics.open_graph " 800x800";
-  Graphics.moveto 400 400;
-  t.pos <- {x = 400.; y=400.; a = 90}
+let init_graphics () =
+  Graphics.open_graph " 800x800";;
 
-let store () =
-  Stack.push t.pos t.states
 
-let restore () =
-  t.pos <- Stack.pop t.states;
-  Graphics.moveto (Float.to_int t.pos.x) (Float.to_int t.pos.y)
+let create_turtle () =
+  let middle_x = (Graphics.size_x ()) / 2 in
+  let middle_y = (Graphics.size_y ()) / 2 in  
+  Graphics.moveto middle_x middle_y;
+  let pos = {x = (Float.of_int middle_x);
+             y = (Float.of_int middle_y);
+             a = 90} in
+  { pos = pos; states = [] };;
 
-let turn a =
+
+let store t =
+  { t with states = t.pos :: t.states }
+
+
+let restore t =
+  match t.states with
+  | [] -> failwith "No more state"
+  | p :: l' -> Graphics.moveto (Float.to_int p.x) (Float.to_int p.y);
+               {pos = p; states = l'}
+
+let turn t angle =
+  let new_pos = { t.pos with a = t.pos.a + angle } in
+  { t with pos = new_pos }
+
+let radian_of_degree deg =
+  deg *. (Float.pi /. 180.);;
+
+let update_pos t n =
   let cur_pos = t.pos in
-  t.pos <- { cur_pos with a = cur_pos.a + a}
-
-
-let update_pos (n : int) : unit =
-  let cur_pos = t.pos in
-  let a' = (Float.of_int cur_pos.a) *. (Float.pi /. 180.) in
+  let a' = radian_of_degree (Float.of_int cur_pos.a) in
   let new_x = (Float.cos a' *. Float.of_int n) +. cur_pos.x in
-  let new_y = (Float.sin a' *. Float.of_int n) +. cur_pos.y in
-  t.pos <- { cur_pos with x = new_x; y = new_y}
+  let new_y = (Float.sin a' *. Float.of_int n) +. cur_pos.y in  
+  let new_pos = { t.pos with x = new_x; y = new_y } in
+  { t with pos = new_pos }
 
 
-let move n =
-  update_pos n;
-  Graphics.moveto (Float.to_int t.pos.x) (Float.to_int t.pos.y)
+let move t n =
+  let t' = update_pos t n in
+  Graphics.moveto (Float.to_int t'.pos.x) (Float.to_int t'.pos.y);
+  t'
 
-let line n =
-  update_pos n;
-  Graphics.lineto (Float.to_int t.pos.x) (Float.to_int t.pos.y)
+let line t n =
+  let t' = update_pos t n in
+  Graphics.lineto (Float.to_int t'.pos.x) (Float.to_int t'.pos.y);
+  t'
 
-let exec c_list = match c_list with
-  | [] -> ()
+let exec t c_list =
+  match c_list with
+  | [] -> t
   | x :: c_list -> match x with
-    | Line n -> line n
-    | Move n -> move n
-    | Turn n -> turn n
-    | Store -> store ()
-    | Restore -> restore ()
+                   | Line n -> line t n
+                   | Move n -> move t n
+                   | Turn n -> turn t n
+                   | Store -> store t
+                   | Restore -> restore t
